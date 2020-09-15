d2af94fd3ed123c68aaaf69100a58bb342e39ca9
Víctor Álvarez <victorpool22@gmail.com>
2020-09-14 23:48:10 -0400

    Agrega detalles de diseño finales

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -6413,7 +6413,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53882" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50586" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
